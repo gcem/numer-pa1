928ca0cee5e1c5fbb984a8abc87aa17033e984c2
gcem <34423323+gcem@users.noreply.github.com>
2023-11-21 18:05:21 +0100

    run function

--- run.py
+++ run.py
@@ -1,9 +1,21 @@
+import numpy as np
 import skimage as ski
 import matplotlib.pyplot as plt
+import pa1
 
 
 def run():
-    ski.imread()
+    bird = ski.io.imread("images/bird.jpg")
+    plane = ski.io.imread("images/plane.jpg")
+
+    y = 24
+    x = 320
+
+    result = np.zeros_like(bird)
+    for rgb in range(3):
+        result[..., rgb] = pa1.clone(plane[..., rgb], bird[..., rgb], y, x)
+
+    ski.io.imshow(result)
 
 
 run()
