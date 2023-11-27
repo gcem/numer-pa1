second task (wrong result)

pa1.py
@@ -6,10 +6,34 @@ def discreteSecondDiff(n: int):
     return sp.diags([-2, 1, 1], offsets=[0, -1, 1], shape=(n, n))
 
 
+def forwardDiff(n: int):
+    return sp.diags([1, -1], offsets=[0, 1], shape=(n, n))
+
+
+def backwardDiff(n: int):
+    return sp.diags([1, -1], offsets=[0, -1], shape=(n, n))
+
+
 def vectorizedLaplace(n: int, m: int):
     return sp.csr_matrix(sp.kron(sp.eye(m), discreteSecondDiff(n)) +
                          sp.kron(discreteSecondDiff(m), sp.eye(n)),
                          dtype=float)
+
+
+def gradient(data: np.ndarray):
+    (n, m) = data.shape
+    return np.dstack(
+        [forwardDiff(n) @ data, data @ forwardDiff(m).transpose()])
+
+
+def squaredNorms(field: np.ndarray):
+    return np.multiply(field, field).sum(axis=2)
+
+
+def divergence(field: np.ndarray):
+    (n, m, _) = field.shape
+    return (backwardDiff(n) @ field[..., 0] +
+            field[..., 1] @ backwardDiff(m).transpose())
 
 
 def setConditionsAtBoundary(originalA: sp.spmatrix,
@@ -51,11 +75,34 @@ def getSystem(source: np.ndarray, target: np.ndarray, y: int, x: int):
     return setConditionsAtBoundary(A, b, target, x, y, source.shape)
 
 
-def clone(source: np.ndarray, target: np.ndarray, y: int, x: int):
+def getSystemForCommonFeatures(source: np.ndarray, target: np.ndarray, y: int,
+                               x: int):
+    (n, m) = source.shape
+    A = vectorizedLaplace(n, m)
+
+    sourceGradient = gradient(source)
+    targetGradient = gradient(target[y:y + n, x:x + m])
+
+    targetFeatures = squaredNorms(targetGradient) > squaredNorms(
+        sourceGradient)
+    maxGradient = sourceGradient.copy()
+    maxGradient[targetFeatures, ...] = targetGradient[targetFeatures]
+
+    b = divergence(maxGradient).flatten('F').transpose()
+
+    return setConditionsAtBoundary(A, b, target, x, y, source.shape)
+
+
+def clone(source: np.ndarray,
+          target: np.ndarray,
+          y: int,
+          x: int,
+          method: str | None = None):
     sourceF = source.astype(float)
     targetF = target.astype(float)
 
-    (A, b) = getSystem(sourceF, targetF, y, x)
+    getSystemFunction = getSystemForCommonFeatures if method == "common" else getSystem
+    (A, b) = getSystemFunction(sourceF, targetF, y, x)
     solution = sp.linalg.spsolve(A, b)
     # solution, info = sp.linalg.cg(A, b, maxiter=5000)
     # solution, istop, iter, *_ = sp.linalg.lsqr(A, b)