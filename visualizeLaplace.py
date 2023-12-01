eb2d34f400fd50cf5613b2f073fd89af1b3a292d
gcem <34423323+gcem@users.noreply.github.com>
2023-12-01 15:19:29 +0100

    code to visualize laplace operator

--- visualizeLaplace.py
+++ visualizeLaplace.py
@@ -6,34 +6,32 @@ import pa1
 
 def imshowThreeColors(matrix):
     (n, m) = matrix.shape
-    ski.io.imshow(np.stack([(matrix * 255).round().clip(0, 255),
-                            np.zeros_like(matrix),
-                            np.zeros_like(matrix)],
-                           axis=2).astype('uint8'),
-                  cmap='grey',
-                  plugin="matplotlib")
-    # aspectRatio = m / n
+    # # alternative:
+    # ski.io.imshow(matrix, plugin="matplotlib")
 
-    # # prepare figure
-    # height = 400  # px
-    # legendWidthRatio = 10
-    # fig = plt.figure()
-    # fig.subplots(1, 2, width_ratios=[legendWidthRatio, 1])
+    aspectRatio = m / n
 
-    # # plot data
-    # ax = fig.axes[0]
-    # ax.set_title(f"Vektorisierter Laplace-Operator ({n}, {m})")
-    # ax.imshow(matrix, extent=(0, m, n, 0), interpolation=None, cmap='gray')
+    # prepare figure
+    legendWidthRatio = 10
+    fig = plt.figure()
+    fig.subplots(1, 2, width_ratios=[legendWidthRatio, 1])
 
-    # # plot the legend
-    # axColor = fig.axes[1]
-    # gradient = np.linspace(1, 0, 3).repeat(100)
-    # gradient = np.vstack((gradient, gradient)).transpose()
-    # axColor.imshow(gradient, aspect=1 / 10, cmap='gray')
-    # axColor.set_title("Eintrag der Matrix")
-    # axColor.set_yticks(np.linspace(0, gradient.shape[0], 7)[1::2])
-    # axColor.set_yticklabels(["-4", "1", "0"])
-    # axColor.xaxis.set_visible(False)
+    # plot the data
+    ax = fig.axes[0]
+    ax.set_title(f"Vektorisierter Laplace-Operator ({n}, {m})")
+    ax.imshow(matrix, extent=(0, m, n, 0), interpolation=None, cmap='gray')
+    ax.minorticks_on()
+    ax.grid(True, 'both')
+
+    # plot the legend
+    axColor = fig.axes[1]
+    dummyData = np.array([1, 0, -4]).repeat(100)
+    dummyData = np.vstack((dummyData, dummyData)).transpose()
+    axColor.imshow(dummyData, aspect=1 / 10, cmap='gray')
+    axColor.set_title("Eintrag der Matrix")
+    axColor.set_yticks(np.linspace(0, dummyData.shape[0], 7)[1::2])
+    axColor.set_yticklabels(["1", "0", "-4"])
+    axColor.xaxis.set_visible(False)
 
     plt.show()
 
@@ -41,7 +39,7 @@ def imshowThreeColors(matrix):
 def visualizeLaplace():
     laplace = pa1.vectorizedLaplace(5, 7).toarray()
     # display the result
-    imshowThreeColors(abs(laplace / 4.))
+    imshowThreeColors(laplace)
 
 
 if __name__ == "__main__":
